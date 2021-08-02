661bcd5e4c86693d078d37b8ed321933cb97c277
Luca <83292494+montaooo@users.noreply.github.com>
2021-08-02 17:45:44 +0200

    Update IntroScreen.java

--- IntroScreen.java
+++ IntroScreen.java
@@ -11,9 +11,11 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.scenes.scene2d.Stage;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.utils.viewport.FitViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
+//import org.graalvm.compiler.nodes.calc.IntegerDivRemNode;
 
 public class IntroScreen implements Screen {
 
@@ -36,12 +38,15 @@ public class IntroScreen implements Screen {
         Label nameLabel = new Label("NOME GIOCO", font);
         Label playLabel = new Label("PRESS ENTER TO PLAY", font);
         Label aboutLabel = new Label("PRESS A FOR ABOUT", font);
+        Label OptionsLabel = new Label("PRESS Z FOR OPTIONS", font);
 
         table.add(nameLabel).expandX();
         table.row();
         table.add(playLabel).expandX().padTop(10f);
         table.row();
         table.add(aboutLabel).expandX();
+        table.row();
+        table.add(OptionsLabel).expandX();
 
         stage.addActor(table);
 
@@ -54,7 +59,6 @@ public class IntroScreen implements Screen {
 
     @Override
     public void show() {
-
     }
 
     @Override
@@ -71,12 +75,16 @@ public class IntroScreen implements Screen {
             dispose();
         }
 
+        if(Gdx.input.isKeyPressed(Input.Keys.Z)){
+            game.setScreen(new Options((MyGame) game));
+            dispose();
+        }
+
         /*
         if(Gdx.input.justTouched()) {
             game.setScreen(new PlayState((MyGame) game));
             dispose();
         }
-
          */
         Gdx.gl.glClearColor(0, 0, 0, 1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
